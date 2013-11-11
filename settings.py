d6c88fe15ca56d9d364046a70b803678ac99dbf3
Andy Newton <amn@ecs.soton.ac.uk>
2013-11-11 00:55:55 +0000

    add settings file

---
+++ settings.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+
+# Server-side plugin directory
+PLUGIN_DIR = './plugins'
+
+# Where static files are stored
+STATIC_FILES = './static'
+
+# Directory for the user "database" (json files for use with Cork)
+USERDB_DIR = './userdb'
+
+# Where to store the rules/outcomes database
+DB_PATH = 'quenb.db'
