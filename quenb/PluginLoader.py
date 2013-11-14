bc641790e5d57dac55732bd541ae7b51bebc4c67
Andy Newton <amn@ecs.soton.ac.uk>
2013-11-14 22:35:55 +0000

    Add function to list all plugins

--- quenb/PluginLoader.py
+++ quenb/PluginLoader.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # From http://www.daniweb.com/software-development/python/code/238387/
-import glob, imp
+import glob, imp, types
 from os.path import join, basename, splitext
 
 # Scans a plugin directory for plugins and loads them, returning a dict name => module.
@@ -13,4 +13,21 @@ def _load(path):
     name, ext = splitext(basename(path))
     return name, imp.load_source(name, path)
 
+# Gets a list of all functions in the structure:
+# {
+#   module_name: {
+#     function_name : function_object,
+#   }
+# }
+def listAllFunctions(plugindir):
+    all_modules = {}
 
+    # List of all plugin modules available
+    modules = importPlugins(plugindir)
+
+    for module_name in modules:
+        module_obj = modules[module_name]
+        all_modules[module_name] = dict(
+          [(fname, fun) for (fname,fun) in module_obj.__dict__.iteritems() if type(fun) == types.FunctionType]
+        )
+    return all_modules
